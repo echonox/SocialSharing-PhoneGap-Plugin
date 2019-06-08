edf27345aadf7ab12da18bafff35e2cd7af654ee
Jack <angelkomyou@gmail.com>
2019-06-08 08:07:28 +0200

    Fatal Exception: java.lang.RuntimeException Unable to start receiver nl.xservices.plugins.ShareChooserPendingIntent: java.lang.NullPointerException: Attempt to invoke virtual method 'java.lang.Object android.os.BaseBundle.get(java.lang.String)' on a null object reference

--- src/android/nl/xservices/plugins/ShareChooserPendingIntent.java
+++ src/android/nl/xservices/plugins/ShareChooserPendingIntent.java
@@ -1,16 +1,18 @@
 package nl.xservices.plugins;
 
+import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.os.Build;
+
+import java.util.Objects;
 
 public class ShareChooserPendingIntent extends BroadcastReceiver {
     public static String chosenComponent = null;
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP_MR1)
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getExtras() != null) {
-            ShareChooserPendingIntent.chosenComponent = intent.getExtras().get(Intent.EXTRA_CHOSEN_COMPONENT).toString();
-        }
+        ShareChooserPendingIntent.chosenComponent = Objects.requireNonNull(intent.getExtras()).get(Intent.EXTRA_CHOSEN_COMPONENT).toString();
     }
 }
